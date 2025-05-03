c50f121fc48e2fe337c35428874f6cf9f06ed6dc
Lee Connell <lee.a.connell@gmail.com>
2025-05-03 18:35:06 -0400

    cleanup

--- src/lib/api/apis/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ItemsApi';
-export * from './LoginApi';
-export * from './PrivateApi';
-export * from './UsersApi';
-export * from './UtilsApi';
--- src/lib/api/login.ts
+++
@@ -1,8 +0,0 @@
-import { LoginApi } from '$lib/api/apis';
-import { apiConfig } from './client';
-
-const loginApi = new LoginApi(apiConfig);
-
-export function loginUser(username: string, password: string) {
-    return loginApi.loginLoginAccessToken({ username, password, grantType: 'password' });
-}
