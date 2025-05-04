c42c7cec8038cb6cea4a645647b6971a0a2e97c5
Lee Connell <lee.a.connell@gmail.com>
2025-05-03 21:37:33 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let count = $state(0);
+
+	function increment(): void {
+		count += 1;
+	}
+</script>
+
+<h1 class="mb-4 text-3xl font-bold">Welcome to SvelteKit 2</h1>
+<button class="btn btn-primary" onclick={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+type Config = import('tailwindcss').Config;
+
+const config: Config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+};
+
+export default config;
