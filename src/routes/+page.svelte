94e899f531abe3c3e3d0a830a2f0e250d9f0e612
Lee Connell <lee.a.connell@gmail.com>
2025-05-05 13:47:13 -0400

    centered landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,11 @@
 	}
 </script>
 
-<h1 class="mb-4 text-3xl font-bold">Welcome to SvelteKit 2</h1>
-<button class="btn btn-primary" onclick={increment}>
-	Clicked {count}
-	{count === 1 ? 'time' : 'times'}
-</button>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center min-h-screen gap-4 text-center">
+	<h1 class="text-3xl font-bold">Welcome to SvelteKit 2</h1>
+	<button class="btn btn-primary" onclick={increment}>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	</button>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
